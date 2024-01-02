794cf66793aa66fe7b86502ac5da71bf14eb954e
transaero21 <transaero21@elseboot.ru>
2024-01-02 13:55:37 +0300

    android: Death

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
 }
 
 buildscript {
     dependencies {
         classpath(libs.kotlin.gradle)
-        classpath(libs.android.gradle)
     }
 }
